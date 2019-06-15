201e7dbae6834c72d5bdf9cceb72a21405c33e5a
Jakub Błach <jakubblach@gmail.com>
2019-06-16 01:53:05 +0200

    remove

--- src/main/kotlin/plugin/Icons.kt
+++ src/main/kotlin/plugin/Icons.kt
@@ -5,6 +5,5 @@ import com.intellij.openapi.util.IconLoader
 object Icons {
 
     @JvmField val SAMPLE_ICON = IconLoader.getIcon("logo.svg")
-    @JvmField val FUNC_ICON = IconLoader.getIcon("f.png")
 
 }
