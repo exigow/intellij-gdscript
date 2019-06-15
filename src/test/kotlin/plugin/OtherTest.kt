58138b0c3d6c4b7a8b5b0c2eb0e3a17eef690c02
Jakub Błach <jakubblach@gmail.com>
2019-06-15 22:53:04 +0200

    add test

---
+++ src/test/kotlin/plugin/OtherTest.kt
@@ -0,0 +1,30 @@
+package plugin
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.tree.TokenSet
+import com.intellij.testFramework.ParsingTestCase
+
+class OtherTest : ParsingTestCase("", "GDScript", SampleParserDefinition()) {
+
+    fun `test single var definition`() {
+        val tree = convertToSyntaxTree("var x = 1")
+        printTree(tree)
+    }
+
+    fun `test multiple var definitions`() {
+        val tree = convertToSyntaxTree("var x = 1\nvar y = 2")
+        printTree(tree)
+    }
+
+    private fun convertToSyntaxTree(code: String): ASTNode {
+        val file = createPsiFile("a.sample", code)
+        ensureParsed(file)
+        return file.node.getChildren(TokenSet.ANY)[0]
+    }
+
+    private fun printTree(tree: ASTNode) {
+        val toShow = ASTNodePrinter.printDirectoryTree(tree)
+        println(toShow)
+    }
+
+}
