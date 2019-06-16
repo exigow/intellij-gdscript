ee698bbefc260ac63b3dff5d46c4eefa4b2422b5
Jakub Błach <jakubblach@gmail.com>
2019-06-16 18:57:20 +0200

    refactor

--- src/main/kotlin/plugin/GDScriptParserDefinition.kt
+++ src/main/kotlin/plugin/GDScriptParserDefinition.kt
@@ -2,13 +2,11 @@ package plugin
 
 import com.intellij.lang.ASTNode
 import com.intellij.lang.ParserDefinition
-import com.intellij.lang.PsiBuilder
 import com.intellij.lang.PsiParser
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.project.Project
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.IFileElementType
 import com.intellij.psi.tree.TokenSet
@@ -18,16 +16,11 @@ import org.antlr.intellij.adaptor.lexer.RuleIElementType
 import org.antlr.intellij.adaptor.lexer.TokenIElementType
 import org.antlr.intellij.adaptor.parser.ANTLRParserAdaptor
 import org.antlr.intellij.adaptor.psi.ANTLRPsiNode
-import plugin.parser.GDScriptLanguageLexer
-import plugin.parser.GDScriptLanguageParser
-import plugin.psi.ArgdefSubtree
-import plugin.psi.BlockSubtree
-import plugin.psi.CallSubtree
-import plugin.psi.FunctionSubtree
-import plugin.psi.GDScriptPsiFileRoot
-import plugin.psi.VardefSubtree
 import org.antlr.v4.runtime.Parser
 import org.antlr.v4.runtime.tree.ParseTree
+import plugin.parser.GDScriptLanguageLexer
+import plugin.parser.GDScriptLanguageParser
+import plugin.psi.*
 
 class GDScriptParserDefinition : ParserDefinition {
 
@@ -49,90 +42,44 @@ class GDScriptParserDefinition : ParserDefinition {
         }
     }
 
-    override fun getWhitespaceTokens() = WHITESPACE
+    override fun getWhitespaceTokens(): TokenSet = WHITESPACE
 
-    override fun getCommentTokens() = COMMENTS
+    override fun getCommentTokens(): TokenSet = COMMENTS
 
-    override fun getStringLiteralElements() = STRING
+    override fun getStringLiteralElements(): TokenSet = STRING
 
     override fun getFileNodeType() = FILE
 
-    /** Create the root of your PSI tree (a PsiFile).
-     *
-     * From IntelliJ IDEA Architectural Overview:
-     * "A PSI (Program Structure Interface) file is the root of a structure
-     * representing the contents of a file as a hierarchy of elements
-     * in a particular programming language."
-     *
-     * PsiFile is to be distinguished from a FileASTNode, which is a parse
-     * tree node that eventually becomes a PsiFile. From PsiFile, we can get
-     * it back via: [PsiFile.getNode].
-     */
-    override fun createFile(viewProvider: FileViewProvider): PsiFile {
-        return GDScriptPsiFileRoot(viewProvider)
+    override fun createFile(viewProvider: FileViewProvider) = GDScriptPsiFileRoot(viewProvider)
+
+    override fun createElement(node: ASTNode): PsiElement {
+        val rule = node.elementType as RuleIElementType
+        return convertRuleToPsi(node, rule)
     }
 
-    /** Convert from *NON-LEAF* parse node (AST they call it)
-     * to PSI node. Leaves are created in the AST factory.
-     * Rename re-factoring can cause this to be
-     * called on a TokenIElementType since we want to rename ID nodes.
-     * In that case, this method is called to create the root node
-     * but with ID type. Kind of strange, but we can simply create a
-     * ASTWrapperPsiElement to make everything work correctly.
-     *
-     * RuleIElementType.  Ah! It's that ID is the root
-     * IElementType requested to parse, which means that the root
-     * node returned from parsetree->PSI conversion.  But, it
-     * must be a CompositeElement! The adaptor calls
-     * rootMarker.done(root) to finish off the PSI conversion.
-     * See [ANTLRParserAdaptor.parse]
-     *
-     * If you don't care to distinguish PSI nodes by type, it is
-     * sufficient to create a [ANTLRPsiNode] around
-     * the parse tree node
-     */
-    override fun createElement(node: ASTNode): PsiElement {
-        val elType = node.elementType
-        if (elType is TokenIElementType) {
-            return ANTLRPsiNode(node)
-        }
-        if (elType !is RuleIElementType) {
-            return ANTLRPsiNode(node)
-        }
-        when (elType.ruleIndex) {
-            GDScriptLanguageParser.RULE_function -> return FunctionSubtree(node, elType)
-            GDScriptLanguageParser.RULE_vardef -> return VardefSubtree(node, elType)
-            GDScriptLanguageParser.RULE_formal_arg -> return ArgdefSubtree(node, elType)
-            GDScriptLanguageParser.RULE_block -> return BlockSubtree(node)
-            GDScriptLanguageParser.RULE_call_expr -> return CallSubtree(node)
-            else -> return ANTLRPsiNode(node)
+    private fun convertRuleToPsi(node: ASTNode, rule: RuleIElementType): ANTLRPsiNode {
+        return when (rule.ruleIndex) {
+            GDScriptLanguageParser.RULE_function -> FunctionSubtree(node, rule)
+            GDScriptLanguageParser.RULE_vardef -> VardefSubtree(node, rule)
+            GDScriptLanguageParser.RULE_formal_arg -> ArgdefSubtree(node, rule)
+            GDScriptLanguageParser.RULE_block -> BlockSubtree(node)
+            GDScriptLanguageParser.RULE_call_expr -> CallSubtree(node)
+            else -> ANTLRPsiNode(node)
         }
     }
 
     companion object {
         val FILE = IFileElementType(GDScriptLanguage)
-
         var ID: TokenIElementType
 
         init {
-            PSIElementTypeFactory.defineLanguageIElementTypes(GDScriptLanguage,
-                    GDScriptLanguageParser.tokenNames,
-                    GDScriptLanguageParser.ruleNames)
+            PSIElementTypeFactory.defineLanguageIElementTypes(GDScriptLanguage, GDScriptLanguageParser.tokenNames, GDScriptLanguageParser.ruleNames)
             val tokenIElementTypes = PSIElementTypeFactory.getTokenIElementTypes(GDScriptLanguage)
             ID = tokenIElementTypes[GDScriptLanguageLexer.ID]
         }
 
-        val COMMENTS = PSIElementTypeFactory.createTokenSet(
-                GDScriptLanguage,
-                GDScriptLanguageLexer.COMMENT,
-                GDScriptLanguageLexer.LINE_COMMENT)
-
-        val WHITESPACE = PSIElementTypeFactory.createTokenSet(
-                GDScriptLanguage,
-                GDScriptLanguageLexer.WS)
-
-        val STRING = PSIElementTypeFactory.createTokenSet(
-                GDScriptLanguage,
-                GDScriptLanguageLexer.STRING)
+        val COMMENTS = PSIElementTypeFactory.createTokenSet(GDScriptLanguage, GDScriptLanguageLexer.COMMENT, GDScriptLanguageLexer.LINE_COMMENT)
+        val WHITESPACE = PSIElementTypeFactory.createTokenSet(GDScriptLanguage, GDScriptLanguageLexer.WS)
+        val STRING = PSIElementTypeFactory.createTokenSet(GDScriptLanguage, GDScriptLanguageLexer.STRING)
     }
 }
