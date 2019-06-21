dd47c5409944e69408db158d8f6acc61ed7906a3
Jakub Błach <jakubblach@gmail.com>
2019-06-22 00:51:32 +0200

    gradle build fix

--- src/test/kotlin/plugin/GDScriptTest.kt
+++ src/test/kotlin/plugin/GDScriptTest.kt
@@ -3,32 +3,38 @@ package plugin
 import com.intellij.psi.PsiElement
 import com.intellij.testFramework.ParsingTestCase
 import org.antlr.intellij.adaptor.xpath.XPath
+import plugin.ASTNodePrinter.printDirectoryTree
 
 class GDScriptTest : ParsingTestCase("", "GDScript", GDScriptParserDefinition()) {
 
-    fun `test expression statement`() {
-        val psi = parse("a = 32")
+    fun `test complex`() {
+        val psi = parse(
+                "hello\n" +
+                "if test:\n" +
+                "    test\n" +
+                "    while test:\n" +
+                "        olla\n" +
+                "        olla\n" +
+                "dolly\n")
         assertSelectionExists(psi, "script/statement/expression")
     }
 
-    fun `test call function expression`() {
-        val psi = parse("ping()")
-        assertSelectionExists(psi, "script/statement/call_function_expression")
+    fun `test simple newlines`() {
+        val psi = parse("one\ntwo\n")
+        assertSelectionExists(psi, "script/statement/expression")
     }
 
     private fun parse(code: String): PsiElement {
         val file = createPsiFile("script.gd", code)
         ensureParsed(file)
-        return file.node.lastChildNode.psi // skip file/root node
+        return file.node.psi
     }
 
     private fun assertSelectionExists(psi: PsiElement, selector: String) {
-        println("PSI tree:\n${toPrettyTreeString(psi)}")
+        println("PSI tree:\n${printDirectoryTree(psi.node)}")
         val result = XPath.findAll(GDScript, psi, selector) // throws error when selection is invalid
         if (result.isEmpty())
             fail("Invalid selector $selector")
     }
-
-    private fun toPrettyTreeString(psi: PsiElement) = ASTNodePrinter.printDirectoryTree(psi.node)
 
 }
