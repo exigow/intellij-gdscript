4fdac8127208a9e5c2d4fab3bd3a5561a786b67c
Jakub Błach <jakubblach@gmail.com>
2019-08-14 00:19:55 +0200

    wip contributor

--- src/main/kotlin/plugin/GDScriptCompletionContributor.kt
+++ src/main/kotlin/plugin/GDScriptCompletionContributor.kt
@@ -5,27 +5,28 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.patterns.PsiElementPattern
 import com.intellij.psi.PsiElement
+import com.intellij.util.PlatformIcons
 import com.intellij.util.ProcessingContext
 
 
 class GDScriptCompletionContributor : CompletionContributor() {
 
     init {
-        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider("var"))
-        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider("const"))
-        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider("if"))
-        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider("while"))
-        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider("return"))
+        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider(LookupElementBuilder.create("var").withIcon(PlatformIcons.METHOD_ICON)))
+        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider(LookupElementBuilder.create("const")))
+        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider(LookupElementBuilder.create("if")))
+        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider(LookupElementBuilder.create("while")))
+        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider(LookupElementBuilder.create("return")))
+        extend(CompletionType.BASIC, placeEverywhere(), createCompletionProvider(LookupElementBuilder.create("extend")))
     }
 
     private fun placeEverywhere(): PsiElementPattern.Capture<PsiElement> {
-        return PlatformPatterns.psiElement().withLanguage(GDScript)
+        return PlatformPatterns.psiElement()
     }
 
-    private fun createCompletionProvider(name: String) = object : CompletionProvider<CompletionParameters>() {
+    private fun createCompletionProvider(lookup: LookupElementBuilder) = object : CompletionProvider<CompletionParameters>() {
 
         override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
-            val lookup = LookupElementBuilder.create(name)
             result.addElement(lookup)
         }
 
