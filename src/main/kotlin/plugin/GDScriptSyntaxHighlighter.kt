c2bd06a0fd2df0cd3c49e4655fd8592379a34a7e
Jakub Błach <jakubblach@gmail.com>
2019-08-15 12:18:29 +0200

    simplify highlighter

--- src/main/kotlin/plugin/GDScriptSyntaxHighlighter.kt
+++ src/main/kotlin/plugin/GDScriptSyntaxHighlighter.kt
@@ -1,6 +1,5 @@
 package plugin
 
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.psi.tree.IElementType
@@ -8,27 +7,19 @@ import org.antlr.intellij.adaptor.lexer.ANTLRLexerAdaptor
 import org.antlr.intellij.adaptor.lexer.TokenIElementType
 import plugin.parser.GDScriptLexer
 import plugin.parser.GDScriptLexer.*
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as DefaultColor
 
 
 class GDScriptSyntaxHighlighter : SyntaxHighlighterBase() {
 
     override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> {
         return if (tokenType !is TokenIElementType) EMPTY else when (tokenType.antlrTokenType) {
-            NUMBER -> pack(DefaultLanguageHighlighterColors.NUMBER)
-            STRING -> pack(DefaultLanguageHighlighterColors.STRING)
-            PARAMETER -> pack(DefaultLanguageHighlighterColors.PARAMETER)
-            IF,
-            WHILE,
-            EXTENDS,
-            VAR,
-            CONST,
-            FUNC,
-            RETURN,
-            TRUE,
-            FALSE,
-            PASS -> pack(DefaultLanguageHighlighterColors.KEYWORD)
-            CLASS_NAME -> pack(DefaultLanguageHighlighterColors.CLASS_NAME)
-            LINE_COMMENT -> pack(DefaultLanguageHighlighterColors.LINE_COMMENT)
+            NUMBER -> pack(DefaultColor.NUMBER)
+            STRING, UNTERMINATED_STRING -> pack(DefaultColor.STRING)
+            PARAMETER -> pack(DefaultColor.PARAMETER)
+            IF, WHILE, EXTENDS, VAR, CONST, FUNC, RETURN, TRUE, FALSE, PASS -> pack(DefaultColor.KEYWORD)
+            CLASS_NAME -> pack(DefaultColor.CLASS_NAME)
+            LINE_COMMENT -> pack(DefaultColor.LINE_COMMENT)
             else -> EMPTY
         }
     }
