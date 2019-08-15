dce99c32428221e494b5c9531396bab39d66b0e9
Jakub Błach <jakubblach@gmail.com>
2019-08-15 17:45:14 +0200

    simplify file code

--- src/main/kotlin/plugin/GDScriptFileType.kt
+++ src/main/kotlin/plugin/GDScriptFileType.kt
@@ -5,13 +5,11 @@ import plugin.icons.GDScriptIconFactory
 
 object GDScriptFileType : LanguageFileType(GDScript) {
 
-    const val FILE_EXTENSION = "gd"
-
     override fun getName() = GDScript.displayName
 
     override fun getDescription() = "todo description"
 
-    override fun getDefaultExtension() = FILE_EXTENSION
+    override fun getDefaultExtension() = "gd"
 
     override fun getIcon() = GDScriptIconFactory.createLogo()
 
