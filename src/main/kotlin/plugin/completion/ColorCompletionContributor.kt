a6ac1e38b30e328a33477c22ccd313b00f26f7c8
Jakub Błach <jakubblach@gmail.com>
2019-08-23 11:40:23 +0200

    refactor color completion impl

--- src/main/kotlin/plugin/completion/ColorCompletionContributor.kt
+++ src/main/kotlin/plugin/completion/ColorCompletionContributor.kt
@@ -5,6 +5,7 @@ import com.intellij.codeInsight.completion.CompletionType.BASIC
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns.psiElement
 import plugin.completion.deserialization.DocumentationDeserializer.deserializeResourceResource
+import plugin.completion.deserialization.models.Constant
 import plugin.completion.deserialization.utilities.ColorParser
 import plugin.completion.utilities.LookupCompletionProvider
 import plugin.icons.IconFactory
@@ -14,14 +15,19 @@ class ColorCompletionContributor : CompletionContributor() {
 
     init {
         val doc = deserializeResourceResource("/docs/Color.xml")
-        for (constant in doc.constants!!) {
-            val name = constant.name
-            val color = ColorParser.parse(constant.value)
-            val colorIcon = createIcon(color)
-            val typeText = createTypeText(color)
-            val lookup = LookupElementBuilder.create(name).withIcon(colorIcon).withTypeText(typeText)
+        for (colorConstant in doc.constants!!) {
+            val lookup = createLookupFromConstant(colorConstant)
             extend(BASIC, psiElement(), LookupCompletionProvider(lookup))
         }
+    }
+
+    private fun createLookupFromConstant(constant: Constant): LookupElementBuilder {
+        val color = ColorParser.parse(constant.value)
+        val icon = createIcon(color)
+        val typeText = createTypeText(color)
+        return LookupElementBuilder.create(constant.name)
+            .withIcon(icon)
+            .withTypeText(typeText)
     }
 
     private fun createIcon(color: Color) = IconFactory.createColorIcon(color)
