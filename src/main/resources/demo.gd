09ceacfbc8885204aefa7d7a97d9d5c726fe4a6c
Jakub Błach <jakubblach@gmail.com>
2019-08-26 23:38:03 +0200

    upgrade demo

--- src/main/resources/demo.gd
+++ src/main/resources/demo.gd
@@ -3,11 +3,14 @@ class_name Hero, "res://interface/icons/hero.png"
 export(Texture) var face
 const MAX_HP = 200
 var hp = 100.0 # Health
+var item = Potion()
 enum Class {ROGUE, KNIGHT, WIZARD}
 """
 Roses are red
 Violets are blue
 """
-var color = blueviolet
+var cloth_color: Color = blueviolet
 func get_damage():
-    return 0x8F51
+    w = weapon.damage
+    s = skill.get_one_handed_bonus()
+    return s * w + 0x8F51
