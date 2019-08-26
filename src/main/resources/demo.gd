2b3007b64e76b87971986b26f709e4177b31870a
Jakub Błach <jakubblach@gmail.com>
2019-08-26 23:28:50 +0200

    add hex to demo

--- src/main/resources/demo.gd
+++ src/main/resources/demo.gd
@@ -2,7 +2,7 @@ extends Node
 class_name Hero, "res://interface/icons/hero.png"
 export(Texture) var face
 const MAX_HP = 200
-var hp = 100 # Health
+var hp = 100.0 # Health
 enum Class {ROGUE, KNIGHT, WIZARD}
 """
 Roses are red
@@ -10,4 +10,4 @@ Violets are blue
 """
 var color = blueviolet
 func get_damage():
-    return 0
+    return 0x8F51
