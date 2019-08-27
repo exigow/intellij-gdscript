a4a296cb344604c8d7161d538c61911780a857c3
Jakub Błach <jakubblach@gmail.com>
2019-08-27 21:39:13 +0200

    replace groovy with kotlin (build.gradle -> build.gradle.kt)

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    java
+    antlr
+    id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    id("org.jetbrains.intellij").version("0.4.9")
+    id("de.undercouch.download").version("4.0.0")
+}
+
+dependencies {
+    antlr("org.antlr:antlr4:4.7.2")
+    compile("com.yuvalshavit:antlr-denter:1.1")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
+    compile("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.9.9")
+    implementation("org.antlr:antlr4-intellij-adaptor:0.1")
+    testCompile("junit:junit:4.11")
+}
+
+val generatedJavaLocation = "src/main/java-gen"
+val generatedResourceLocation = "src/main/resources-gen"
+
+sourceSets["main"].java.srcDir(generatedJavaLocation)
+sourceSets["main"].resources.srcDir(generatedResourceLocation)
+
+intellij {
+    version = "2018.3"
+    downloadSources = true
+    updateSinceUntilBuild = false
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    compileJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+    }
+    generateGrammarSource {
+        val path = "plugin/parser/grammar"
+        outputDirectory = file("$generatedJavaLocation/$path")
+        val pack = path.replace("/", ".")
+        arguments = listOf("-package", pack, "-no-listener", "-no-visitor")
+    }
+
+}
