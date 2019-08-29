e88085b958f17da17763b0fe31dc4d0a9b4f28f2
Jakub Błach <jakubblach@gmail.com>
2019-08-29 23:09:54 +0200

    refactoring deserializer

--- src/main/kotlin/gdscript/completion/utilities/BuiltInClassDeserializer.kt
+++ src/main/kotlin/gdscript/completion/utilities/BuiltInClassDeserializer.kt
@@ -2,25 +2,24 @@ package gdscript.completion.utilities
 
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import gdscript.completion.utilities.models.Documentation
 import org.intellij.lang.annotations.Language
-import java.io.File
 
 
 object BuiltInClassDeserializer {
 
     fun deserializeResource(resourceName: String): Documentation {
-        val text = BuiltInClassDeserializer::class.java.getResource(resourceName).readText()
+        val text = this::class.java.getResource(resourceName).readText()
         return deserializeText(text)
     }
 
     fun deserializeText(@Language("xml") xml: String): Documentation {
-        val fixedXml = fixTagsWithOnlyWhitespaces(xml)
+        val fixedXml: String = fixTagsWithOnlyWhitespaces(xml)
         return configureMapper().readValue(fixedXml, Documentation::class.java)
     }
 
@@ -30,19 +29,21 @@ object BuiltInClassDeserializer {
     private fun configureMapper() = XmlMapper()
         .registerModule(KotlinModule())
         .registerModule(StringTrimModule())
-        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .disable(FAIL_ON_UNKNOWN_PROPERTIES)
 
     private class StringTrimModule : SimpleModule() {
 
         init {
-            val trimDeserializer = object : StdScalarDeserializer<String>(String::class.java) {
-
-                override fun deserialize(parser: JsonParser, context: DeserializationContext)
-                    = parser.valueAsString.trim()
-
-            }
-            addDeserializer(String::class.java, trimDeserializer)
+            addDeserializer(String::class.java, StringDeserializer())
         }
+
+    }
+
+    private class StringDeserializer : StdScalarDeserializer<String>(String::class.java) {
+
+        override fun deserialize(parser: JsonParser, context: DeserializationContext)
+            = parser.valueAsString.trim()
+
     }
 
 }
