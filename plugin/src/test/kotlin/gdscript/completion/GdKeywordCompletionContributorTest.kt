6a5f1310c840f7b0b5374733f55a50af1ff7873f
Jakub Błach <jakubblach@gmail.com>
2019-09-10 20:15:35 +0200

    Visibility fix

--- plugin/src/test/kotlin/gdscript/completion/GdKeywordCompletionContributorTest.kt
+++ plugin/src/test/kotlin/gdscript/completion/GdKeywordCompletionContributorTest.kt
@@ -12,7 +12,7 @@ class GdKeywordCompletionContributorTest : GdTestBase() {
     fun `test keyword const`() =
         assertCodeCompletionContains("co${CARET}", listOf("const"))
 
-    fun assertCodeCompletionContains(code: String, expectedLookups: List<String>) {
+    private fun assertCodeCompletionContains(code: String, expectedLookups: List<String>) {
         myFixture.configureByText(GdFileType, code)
         myFixture.complete(CompletionType.BASIC, 1)
         val lookups = myFixture.lookupElementStrings.orEmpty()
