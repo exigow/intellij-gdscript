3376fc36d6d74266fb245eb3e4708b553c241611
Jakub Błach <jakubblach@gmail.com>
2019-09-10 19:34:39 +0200

    Add "New File Action" with Class and File templates
    Shortcut: Alt + Insert

---
+++ plugin/src/main/kotlin/gdscript/GdCreateFileAction.kt
@@ -0,0 +1,19 @@
+package gdscript
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+
+class GdCreateFileAction : CreateFileFromTemplateAction("GDScript", "", GdIcons.FILE_ICON) {
+
+    override fun getActionName(directory: PsiDirectory, newName: String, templateName: String)
+        = "Create GDScript File"
+
+    override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
+        builder.setTitle("New GDScript File/Class")
+            .addKind("File", GdIcons.FILE_ICON, "GDScript File")
+            .addKind("Class", GdIcons.FILE_ICON, "GDScript Class")
+    }
+
+}
