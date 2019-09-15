f3b4cfe9c7662131ce9029627dbb9afd30c9a997
Jakub Błach <jakubblach@gmail.com>
2019-09-16 00:59:21 +0200

    Refactor completion contributor :construction:

--- plugin/src/main/kotlin/gdscript/ScriptCompletionContributor.kt
+++ plugin/src/main/kotlin/gdscript/ScriptCompletionContributor.kt
@@ -18,69 +18,18 @@ class ScriptCompletionContributor : CompletionContributor() {
     private val library = Library.load()
 
     init {
-        val languageClass = library.classes.find { it.name == "@GDScript" }!!
-        val languageFunctionLookups = languageClass.methods
-            .map { create(it.name).bold().withIcon(FUNCTION_ICON).withArgumentsTailText(it.arguments).withTypeText(it.type).withInvocationSuffix() }
-        extend(psiElement(), languageFunctionLookups)
-        val languageConstantLookups = languageClass.constants
-            .map { create(it.name).bold().withIcon(FIELD_ICON).withValueTailText(it.value) }
-        extend(psiElement(), languageConstantLookups)
-
-        val otherClasses = library.classes - languageClass
-        val classNameLookups = otherClasses.map { create(it.name).withIcon(CLASS_ICON) }
-        extend(psiElement().afterLeaf("->"), classNameLookups)
-        extend(psiElement().afterLeaf("extends"), classNameLookups)
-        extend(psiElement().afterLeaf(":"), classNameLookups)
-        val fieldLookups = otherClasses.flatMap { it.fields.map { f -> create(f.name).withIcon(VARIABLE_ICON).withTypeText(f.type).bold() } }
-        extend(psiElement().afterLeaf("."), fieldLookups)
-        val setterLookups = otherClasses.flatMap { it.fields.map { f -> create(f.setter).italics().withIcon(METHOD_ICON).withTailText("(value: ${f.type})") } }
-        val getterLookups = otherClasses.flatMap { it.fields.map { f -> create(f.getter).italics().withIcon(METHOD_ICON).withInvocationTailText().withTypeText(f.type).withInvocationSuffix() } }
-        extend(psiElement().afterLeaf("."), setterLookups + getterLookups)
-        val methodLookups = otherClasses.flatMap { it.methods.map { m -> create(m.name).withIcon(METHOD_ICON).withArgumentsTailText(m.arguments).withTypeText(m.type).withInvocationSuffix() } }
-        extend(psiElement().afterLeaf("."), methodLookups)
-        val constantLookups = otherClasses.flatMap { it.constants.map { c -> create(c.name).withIcon(FIELD_ICON).withValueTailText(c.value) } }
-        extend(psiElement().afterLeaf("."), constantLookups)
-
-        val keywordLookups = listOf(
-            create("if").bold().withStatementTailText().withStatementSuffix(),
-            create("elif").bold().withStatementTailText().withStatementSuffix(),
-            create("else").bold().withTailText(":").withSuffix(":", move =  1),
-            create("for").bold().withStatementTailText().withStatementSuffix(),
-            create("while").bold().withStatementTailText().withStatementSuffix(),
-            create("match").bold().withStatementTailText().withStatementSuffix(),
-            create("break").bold(),
-            create("continue").bold(),
-            create("pass").bold(),
-            create("return").bold().withSpaceSuffix(),
-            create("class").bold().withSpaceSuffix(),
-            create("class_name").bold().withSpaceSuffix(),
-            create("extends").bold().withSpaceSuffix(),
-            create("is").bold().withSpaceSuffix(),
-            create("as").bold().withSpaceSuffix(),
-            create("self").bold(),
-            create("tool").bold(),
-            create("signal").bold().withSpaceSuffix(),
-            create("func").bold().withSpaceSuffix(),
-            create("static").bold().withSpaceSuffix(),
-            create("const").bold().withSpaceSuffix(),
-            create("enum").bold().withTailText(" {...}").withSuffix(" {}", move = 2),
-            create("var").bold().withSpaceSuffix(),
-            create("onready").bold().withSpaceSuffix(),
-            create("export").bold().withInvocationTailText().withInvocationSuffix(),
-            create("setget").bold().withSpaceSuffix(),
-            create("breakpoint").bold(),
-            create("load").bold().withInvocationTailText().withInvocationSuffix(),
-            create("preload").bold().withInvocationTailText().withInvocationSuffix(),
-            create("yield").bold().withInvocationTailText().withInvocationSuffix(),
-            create("assert").bold().withInvocationTailText().withInvocationSuffix(),
-            create("remote").bold().withSpaceSuffix(),
-            create("master").bold().withSpaceSuffix(),
-            create("puppet").bold().withSpaceSuffix(),
-            create("remotesync").bold(),
-            create("mastersync").bold(),
-            create("puppetsync").bold()
-        )
-        extend(psiElement(), keywordLookups)
+        val language = library.classes.find { it.name == "@GDScript" }!!
+        extend(psiElement(), language.methods.map { createLanguageFunctionLookup(it) })
+        extend(psiElement(), language.constants.map { createLanguageConstantLookup(it) })
+        val otherClasses = library.classes - language
+        extend(psiElement().afterLeaf("->"), otherClasses.map { createClassLookup(it) })
+        extend(psiElement().afterLeaf("extends"), otherClasses.map { createClassLookup(it) })
+        extend(psiElement().afterLeaf(":"), otherClasses.map { createClassLookup(it) })
+        extend(psiElement().afterLeaf("."), otherClasses.flatMap { c -> c.fields.map { createFieldLookups(it) } })
+        extend(psiElement().afterLeaf("."), otherClasses.flatMap { c -> c.fields.flatMap { listOf(createSetterLookups(it), createGetterLookups(it)) } })
+        extend(psiElement().afterLeaf("."), otherClasses.flatMap { c -> c.methods.map { createMethodLookups(it) } })
+        extend(psiElement().afterLeaf("."), otherClasses.flatMap { c -> c.constants.map { createConstantLookups(it) } })
+        extend(psiElement(), createKeywordLookups())
     }
 
     private fun extend(pattern: PsiElementPattern.Capture<PsiElement>, lookups: List<LookupElement>) {
@@ -91,6 +40,49 @@ class ScriptCompletionContributor : CompletionContributor() {
         }
         return extend(CompletionType.BASIC, pattern, provider)
     }
+
+    private fun createLanguageFunctionLookup(it: Library.Class.Method) = create(it.name)
+        .bold()
+        .withIcon(FUNCTION_ICON)
+        .withArgumentsTailText(it.arguments)
+        .withTypeText(it.type)
+        .withInvocationSuffix()
+
+    private fun createLanguageConstantLookup(it: Library.Class.Constant) = create(it.name)
+        .bold()
+        .withIcon(FIELD_ICON)
+        .withValueTailText(it.value)
+
+    private fun createClassLookup(clazz: Library.Class) = create(clazz.name)
+        .withIcon(CLASS_ICON)
+
+    private fun createFieldLookups(field: Library.Class.Field) = create(field.name)
+        .withIcon(VARIABLE_ICON)
+        .withTypeText(field.type)
+        .bold()
+
+    private fun createSetterLookups(field: Library.Class.Field) = create(field.setter)
+        .italics()
+        .withIcon(METHOD_ICON)
+        .withTailText("(value: ${field.type})")
+
+    private fun createGetterLookups(field: Library.Class.Field) = create(field.getter)
+        .italics()
+        .withIcon(METHOD_ICON)
+        .withInvocationTailText()
+        .withTypeText(field.type)
+        .withInvocationSuffix()
+
+    private fun createMethodLookups(method: Library.Class.Method) = create(method.name)
+        .withIcon(METHOD_ICON)
+        .withArgumentsTailText(method.arguments)
+        .withTypeText(method.type)
+        .withInvocationSuffix()
+
+    private fun createConstantLookups(constant: Library.Class.Constant) = create(constant.name)
+        .withIcon(FIELD_ICON)
+        .withValueTailText(constant.value)
+
 
     private fun LookupElementBuilder.withInvocationTailText() =
         withTailText("()")
@@ -121,5 +113,45 @@ class ScriptCompletionContributor : CompletionContributor() {
         ctx.document.insertString(ctx.selectionEndOffset, suffix)
         EditorModificationUtil.moveCaretRelatively(ctx.editor, move)
     }
+
+    private fun createKeywordLookups() = listOf(
+        create("if").bold().withStatementTailText().withStatementSuffix(),
+        create("elif").bold().withStatementTailText().withStatementSuffix(),
+        create("else").bold().withTailText(":").withSuffix(":", move =  1),
+        create("for").bold().withStatementTailText().withStatementSuffix(),
+        create("while").bold().withStatementTailText().withStatementSuffix(),
+        create("match").bold().withStatementTailText().withStatementSuffix(),
+        create("break").bold(),
+        create("continue").bold(),
+        create("pass").bold(),
+        create("return").bold().withSpaceSuffix(),
+        create("class").bold().withSpaceSuffix(),
+        create("class_name").bold().withSpaceSuffix(),
+        create("extends").bold().withSpaceSuffix(),
+        create("is").bold().withSpaceSuffix(),
+        create("as").bold().withSpaceSuffix(),
+        create("self").bold(),
+        create("tool").bold(),
+        create("signal").bold().withSpaceSuffix(),
+        create("func").bold().withSpaceSuffix(),
+        create("static").bold().withSpaceSuffix(),
+        create("const").bold().withSpaceSuffix(),
+        create("enum").bold().withTailText(" {...}").withSuffix(" {}", move = 2),
+        create("var").bold().withSpaceSuffix(),
+        create("onready").bold().withSpaceSuffix(),
+        create("export").bold().withInvocationTailText().withInvocationSuffix(),
+        create("setget").bold().withSpaceSuffix(),
+        create("breakpoint").bold(),
+        create("load").bold().withInvocationTailText().withInvocationSuffix(),
+        create("preload").bold().withInvocationTailText().withInvocationSuffix(),
+        create("yield").bold().withInvocationTailText().withInvocationSuffix(),
+        create("assert").bold().withInvocationTailText().withInvocationSuffix(),
+        create("remote").bold().withSpaceSuffix(),
+        create("master").bold().withSpaceSuffix(),
+        create("puppet").bold().withSpaceSuffix(),
+        create("remotesync").bold(),
+        create("mastersync").bold(),
+        create("puppetsync").bold()
+    )
 
 }
