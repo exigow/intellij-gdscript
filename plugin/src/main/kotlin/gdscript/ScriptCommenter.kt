a8519330fb5e90597488f276751ace16eb6eff8f
Jakub Błach <jakubblach@gmail.com>
2019-09-16 00:27:36 +0200

    Create const for multiline symbol (""")

--- plugin/src/main/kotlin/gdscript/ScriptCommenter.kt
+++ plugin/src/main/kotlin/gdscript/ScriptCommenter.kt
@@ -4,6 +4,10 @@ import gdscript.adaptors.CommenterAdaptor
 
 class ScriptCommenter : CommenterAdaptor(
     linePrefix = "#",
-    blockPrefix = "\"\"\"",
-    blockPostfix = "\"\"\""
-)
+    blockPrefix = MULTILINE_STRING_MARKER,
+    blockPostfix = MULTILINE_STRING_MARKER
+) {
+    companion object {
+        const val MULTILINE_STRING_MARKER = "\"\"\""
+    }
+}
