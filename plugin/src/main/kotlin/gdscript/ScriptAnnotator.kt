de3de5830432ebdfe3aa6a6dacc14b8655b74e92
Jakub Błach <jakubblach@gmail.com>
2019-09-18 22:30:55 +0200

    Add more annotation features

--- plugin/src/main/kotlin/gdscript/ScriptAnnotator.kt
+++ plugin/src/main/kotlin/gdscript/ScriptAnnotator.kt
@@ -3,7 +3,7 @@ package gdscript
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors.FUNCTION_DECLARATION
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors.*
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
@@ -13,11 +13,21 @@ import org.antlr.intellij.adaptor.lexer.TokenIElementType
 
 class ScriptAnnotator : Annotator {
 
+    private val classNames = Library.load().classes.map { it.name }
+
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element.isLeafToken(IDENTIFIER))
-            if (element.closestPreviousLeaf().text == "func")
+        if (element.isLeaf(IDENTIFIER)) {
+            if (element.afterLeaf("func"))
                 holder.createAnnotationAttribute(element, FUNCTION_DECLARATION)
+            if (element.afterLeaf("const"))
+                holder.createAnnotationAttribute(element, CONSTANT)
+            if (element.afterLeaf("class") || element.afterLeaf("extends") || element.afterLeaf("class_name") || classNames.any { it == element.text })
+                holder.createAnnotationAttribute(element, INTERFACE_NAME)
+        }
     }
+
+    private fun PsiElement.afterLeaf(leaf: String) =
+        closestPreviousLeaf().text == leaf
 
     private fun PsiElement.closestPreviousLeaf() = when (prevSibling) {
         null -> this
@@ -25,8 +35,7 @@ class ScriptAnnotator : Annotator {
         else -> prevSibling
     }
 
-
-    private fun PsiElement.isLeafToken(token: Int): Boolean {
+    private fun PsiElement.isLeaf(token: Int): Boolean {
         if (this is LeafPsiElement) {
             val type = elementType
             if (type is TokenIElementType)
