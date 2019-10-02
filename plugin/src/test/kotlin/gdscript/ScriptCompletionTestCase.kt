17d8a3e7e6d41e93a1388b8fb36ab4b3fe5ab757
Jakub Błach <jakubblach@gmail.com>
2019-10-02 16:23:27 +0200

    Refactor tests

--- plugin/src/test/kotlin/gdscript/ScriptCompletionTestCase.kt
+++ plugin/src/test/kotlin/gdscript/ScriptCompletionTestCase.kt
@@ -1,56 +1,56 @@
 package gdscript
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 
 class ScriptCompletionTestCase : BasePlatformTestCase() {
 
     fun `test variable keyword`() =
-        assertCompletion("va$CARET", "var")
+        assertCompletion("va<caret>", "var")
 
     fun `test constant keyword`() =
-        assertCompletion("con$CARET", "const")
+        assertCompletion("con<caret>", "const")
 
     fun `test class keyword`() =
-        assertCompletion("cl$CARET", "class")
+        assertCompletion("cl<caret>", "class")
 
     fun `test class_name keyword`() =
-        assertCompletion("cl$CARET", "class_name")
+        assertCompletion("cl<caret>", "class_name")
 
     fun `test variable type`() =
-        assertCompletion("var position: Vec$CARET", "Vector2")
+        assertCompletion("var position: Vec<caret>", "Vector2")
 
     fun `test extends type`() =
-        assertCompletion("extends No$CARET", "Node")
+        assertCompletion("extends No<caret>", "Node")
 
     fun `test function type`() =
-        assertCompletion("func get_name() -> $CARET", "String")
+        assertCompletion("func get_name() -> <caret>", "String")
 
     fun `test function argument type`() =
-        assertCompletion("func draw_quad(geometry: $CARET):", "ImmediateGeometry")
+        assertCompletion("func draw_quad(geometry: <caret>):", "ImmediateGeometry")
 
     fun `test language level function`() =
-        assertCompletion("si$CARET", "sin")
+        assertCompletion("si<caret>", "sin")
 
     fun `test constant field`() =
-        assertCompletion("AnimatedTexture.MAX_$CARET", "MAX_FRAMES")
+        assertCompletion("AnimatedTexture.MAX_<caret>", "MAX_FRAMES")
 
     fun `test method`() =
-        assertCompletion("sprite.is_$CARET()", "is_pixel_opaque")
+        assertCompletion("sprite.is_<caret>()", "is_pixel_opaque")
 
     fun `test field setter`() =
-        assertCompletion("compressor.set_back$CARET()", "set_background")
+        assertCompletion("compressor.set_back<caret>()", "set_background")
 
     fun `test field getter`() =
-        assertCompletion("compressor.get_back$CARET()", "get_background")
+        assertCompletion("compressor.get_back<caret>()", "get_background")
 
     private fun assertCompletion(code: String, expectedLookup: String) {
         myFixture.configureByText(ScriptFileType, code)
         myFixture.completeBasic()
-        assertTrue(myFixture.lookupElementStrings!!.contains(expectedLookup))
+        myFixture.checkLookup(expectedLookup)
     }
 
-    companion object {
-        const val CARET = "<caret>"
-    }
+    private fun CodeInsightTestFixture.checkLookup(expectedLookup: String) =
+        assertTrue(lookupElementStrings!!.contains(expectedLookup))
 
 }
