d8f37b78a9c884a9c991b5969af0e9fd62a74ba9
Jakub Błach <jakubblach@gmail.com>
2019-10-06 12:39:16 +0200

    Improve built-in methods completion with auto-brackets

--- plugin/src/main/kotlin/script/completion/BuiltInFunctionCompletionContributor.kt
+++ plugin/src/main/kotlin/script/completion/BuiltInFunctionCompletionContributor.kt
@@ -1,12 +1,10 @@
 package script.completion
 
 import GodotApi
-import com.intellij.codeInsight.completion.CompletionContributor
-import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.completion.CompletionProvider
-import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.completion.CompletionType.BASIC
 import com.intellij.codeInsight.lookup.LookupElementBuilder.create
+import com.intellij.openapi.editor.EditorModificationUtil
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.util.PlatformIcons.FUNCTION_ICON
 import com.intellij.util.ProcessingContext
@@ -24,12 +22,36 @@ class BuiltInFunctionCompletionContributor : CompletionContributor() {
             .find { it.name == "@GDScript" }!!
 
         override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
-            for (method in LANGUAGE_CLASS.methods) {
-                val arguments = method.arguments.joinToString(", ", "(", ")") { "${it.name}: ${it.type}" }
-                val lookup = create(method.name).withIcon(FUNCTION_ICON).withItemTextItalic(true).withTailText(arguments).withTypeText(method.type)
-                result.addElement(lookup)
-            }
+            for (method in LANGUAGE_CLASS.methods)
+                result.addElement(createBuiltInMethodLookup(method))
         }
+
+        private fun createBuiltInMethodLookup(method: GodotApi.Class.Method) = create(method.name)
+            .withIcon(FUNCTION_ICON)
+            .withItemTextItalic(true)
+            .withTailText(method.joinedArguments())
+            .withTypeText(method.type)
+            .withInsertHandler { ctx, _ ->
+                if (ctx.hasNoOpenBraceAfterCaret()) {
+                    ctx.insert("()")
+                    if (method.arguments.isEmpty())
+                        ctx.moveCaret(2)
+                    else
+                        ctx.moveCaret(1)
+                }
+            }
+
+        private fun InsertionContext.hasNoOpenBraceAfterCaret() =
+            file.text[tailOffset] != '('
+
+        private fun InsertionContext.insert(text: String) =
+            document.insertString(selectionEndOffset, text)
+
+        private fun InsertionContext.moveCaret(steps: Int) =
+            EditorModificationUtil.moveCaretRelatively(editor, steps)
+
+        private fun GodotApi.Class.Method.joinedArguments() =
+            arguments.joinToString(", ", "(", ")") { "${it.name}: ${it.type}" }
 
     }
 
