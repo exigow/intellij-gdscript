210fe4d974ad0b0f43b20e99fa103ddea861c517
Jakub Błach <jakubblach@gmail.com>
2019-10-06 11:32:23 +0200

    Change lookup style

--- plugin/src/main/kotlin/script/completion/BuiltInFunctionCompletionContributor.kt
+++ plugin/src/main/kotlin/script/completion/BuiltInFunctionCompletionContributor.kt
@@ -8,6 +8,7 @@ import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.CompletionType.BASIC
 import com.intellij.codeInsight.lookup.LookupElementBuilder.create
 import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.util.PlatformIcons.FUNCTION_ICON
 import com.intellij.util.ProcessingContext
 
 
@@ -25,7 +26,7 @@ class BuiltInFunctionCompletionContributor : CompletionContributor() {
         override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
             for (method in LANGUAGE_CLASS.methods) {
                 val arguments = method.arguments.joinToString(", ", "(", ")") { "${it.name}: ${it.type}" }
-                val lookup = create(method.name).bold().withItemTextItalic(true).withTailText(arguments).withTypeText(method.type)
+                val lookup = create(method.name).withIcon(FUNCTION_ICON).withItemTextItalic(true).withTailText(arguments).withTypeText(method.type)
                 result.addElement(lookup)
             }
         }
