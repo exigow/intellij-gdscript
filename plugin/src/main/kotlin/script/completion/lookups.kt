a0c03cb5fb25ab72c1e0256db1e99c21a65320b8
Jakub Błach <jakubblach@gmail.com>
2019-10-12 23:59:19 +0200

    Refactor lookups catalog

--- plugin/src/main/kotlin/script/completion/lookups.kt
+++ plugin/src/main/kotlin/script/completion/lookups.kt
@@ -8,9 +8,7 @@ import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.codeInsight.lookup.LookupElementBuilder.create
 import com.intellij.openapi.editor.EditorModificationUtil
-import com.intellij.util.PlatformIcons
-import com.intellij.util.PlatformIcons.CLASS_ICON
-import com.intellij.util.PlatformIcons.FUNCTION_ICON
+import com.intellij.util.PlatformIcons.*
 import kotlin.math.min
 
 
@@ -22,37 +20,45 @@ val FUNCTION_CALL_LOOKUPS = LANGUAGE_CLASS.methods.map { createFunctionCall(it) 
 val PRIMITIVE_LOOKUPS = PRIMITIVE_CLASSES.map { createPrimitive(it) }
 
 private fun createClass(it: GodotApi.Class) = create(it.name)
+    .notCaseSensitive()
     .withIcon(CLASS_ICON)
-    .withCaseSensitivity(false)
 
 private fun createConstant(it: GodotApi.Class.Constant) = create(it.name)
-    .withItemTextItalic(true)
+    .notCaseSensitive()
+    .withIcon(VARIABLE_ICON)
     .withTailText(" = ${it.value}")
-    .withIcon(PlatformIcons.VARIABLE_ICON).bold()
+    .italics()
+    .bold()
 
 private fun createConstructorCall(it: GodotApi.Class) = create(it.name)
     .withIcon(CLASS_ICON)
-    .withTailText("()")
     .withTypeText(it.name)
-    .withArgumentsInsertHandler()
+    .withArgumentsTail(emptyList())
+    .withArgumentHandler(emptyList())
 
 private fun createFunctionCall(it: GodotApi.Class.Method) = create(it.name)
     .withIcon(FUNCTION_ICON)
-    .withItemTextItalic(true)
-    .withArgumentsTail(it.arguments)
     .withTypeText(it.type)
-    .withArgumentsInsertHandler(it.arguments)
+    .withArgumentsTail(it.arguments)
+    .withArgumentHandler(it.arguments)
+    .italics()
 
-private fun createPrimitive(it: GodotApi.Class) = create(it.name).bold()
-    .withCaseSensitivity(false)
+private fun createPrimitive(it: GodotApi.Class) = create(it.name)
+    .notCaseSensitive()
+    .withIcon(CLASS_ICON)
+    .bold()
+
+private fun LookupElementBuilder.notCaseSensitive() = withCaseSensitivity(false)
+
+private fun LookupElementBuilder.italics() = withItemTextItalic(true)
 
 private fun LookupElementBuilder.withArgumentsTail(arguments: List<GodotApi.Class.Method.Argument>) =
     withTailText(arguments.joinToString(", ", "(", ")") { "${it.name}: ${it.type}" })
 
-private fun LookupElementBuilder.withArgumentsInsertHandler(arguments: List<GodotApi.Class.Method.Argument> = emptyList()) =
+private fun LookupElementBuilder.withArgumentHandler(arguments: List<GodotApi.Class.Method.Argument>) =
     withInsertHandler { ctx, _ ->
         if (ctx.hasNoOpenBraceAfterCaret()) {
-            ctx.insert("()")
+            ctx.insertParentheses()
             if (arguments.isEmpty())
                 ctx.moveCaret(2)
             else
@@ -63,8 +69,8 @@ private fun LookupElementBuilder.withArgumentsInsertHandler(arguments: List<Godo
 private fun InsertionContext.hasNoOpenBraceAfterCaret() =
     file.text[min(tailOffset, file.text.length - 1)] != '('
 
-private fun InsertionContext.insert(text: String) =
-    document.insertString(selectionEndOffset, text)
+private fun InsertionContext.insertParentheses() =
+    document.insertString(selectionEndOffset, "()")
 
 private fun InsertionContext.moveCaret(steps: Int) =
     EditorModificationUtil.moveCaretRelatively(editor, steps)
