d00c553a60eb4e2664b0fb31dcac2ae33d11cfb9
Jakub Błach <jakubblach@gmail.com>
2019-10-13 20:40:13 +0200

    Refactor self-true-false keywords completion

--- plugin/src/main/kotlin/script/completion/values.kt
+++ plugin/src/main/kotlin/script/completion/values.kt
@@ -4,9 +4,8 @@ import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.util.PlatformIcons.FIELD_ICON
 
-val SELF = createLookup("self")
-val TRUE = createLookup("true")
-val FALSE = createLookup("false")
+val SELF_TRUE_FALSE = listOf("self", "true", "false")
+    .map { createLookup(it) }
 
 private fun createLookup(name: String): LookupElement = LookupElementBuilder.create(name)
     .withIcon(FIELD_ICON)
