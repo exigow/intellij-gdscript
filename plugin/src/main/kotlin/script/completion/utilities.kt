194783427270a29747062d13ee2e7b722baa68ee
Jakub Błach <jakubblach@gmail.com>
2019-10-14 00:38:40 +0200

    Refactor completion

--- plugin/src/main/kotlin/script/completion/utilities.kt
+++ plugin/src/main/kotlin/script/completion/utilities.kt
@@ -1,31 +1,21 @@
 package script.completion
 
 import GodotApi
-import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.codeInsight.completion.AddSpaceInsertHandler
 import com.intellij.codeInsight.completion.util.ParenthesesInsertHandler
-import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.openapi.editor.EditorModificationUtil
-import kotlin.math.min
 
 internal fun LookupElementBuilder.italics() =
     withItemTextItalic(true)
 
-internal fun LookupElementBuilder.withArgumentsTail(arguments: List<GodotApi.Class.Method.Argument> = emptyList()) =
+internal fun LookupElementBuilder.withArgumentsTail(arguments: List<GodotApi.Class.Method.Argument>) =
     withTailText(arguments.joinToString(", ", "(", ")") { "${it.name}: ${it.type}" })
 
-internal fun LookupElementBuilder.withParenthesesInsertHandler(hasArguments: Boolean = false): LookupElementBuilder {
-    val handler = object : ParenthesesInsertHandler<LookupElement>() {
-        override fun placeCaretInsideParentheses(context: InsertionContext, item: LookupElement) = hasArguments
-    }
-    return withInsertHandler(handler)
-}
+internal fun LookupElementBuilder.withParenthesesInsertHandler(hasParameters: Boolean): LookupElementBuilder =
+    if (hasParameters)
+        withInsertHandler(ParenthesesInsertHandler.WITH_PARAMETERS)
+    else
+        withInsertHandler(ParenthesesInsertHandler.NO_PARAMETERS)
 
-internal fun InsertionContext.hasNextCharacter(character: Char) =
-    file.text[min(tailOffset, file.text.length - 1)] == character
-
-internal fun InsertionContext.insert(text: String) =
-    document.insertString(selectionEndOffset, text)
-
-internal fun InsertionContext.moveCaret(steps: Int) =
-    EditorModificationUtil.moveCaretRelatively(editor, steps)
+internal fun LookupElementBuilder.withSpaceInsertHandler(): LookupElementBuilder =
+    withInsertHandler(AddSpaceInsertHandler.INSTANCE_WITH_AUTO_POPUP)
