43d9f427d67de48c3701e746d283289b206dd02e
Jakub Błach <jakubblach@gmail.com>
2019-10-17 22:13:08 +0200

    Refactor ColorTextAttributesKey

--- plugin/src/main/kotlin/gdscript/options/ColorTextAttributeKey.kt
+++ plugin/src/main/kotlin/gdscript/options/ColorTextAttributeKey.kt
@@ -5,24 +5,30 @@ import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributes
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as Default
 
 
-enum class ColorTextAttributeKey(val key: TextAttributesKey) {
+enum class ColorTextAttributeKey(copied: TextAttributesKey) {
 
-    LINE_COMMENT(createTextAttributesKey("GDSCRIPT_LINE_COMMENT", Default.LINE_COMMENT)),
-    STRING(createTextAttributesKey("GDSCRIPT_STRING", Default.STRING)),
-    NUMBER(createTextAttributesKey("GDSCRIPT_NUMBER", Default.NUMBER)),
-    KEYWORD(createTextAttributesKey("GDSCRIPT_KEYWORD", Default.KEYWORD)),
-    IDENTIFIER(createTextAttributesKey("GDSCRIPT_IDENTIFIER", Default.IDENTIFIER)),
-    NODE(createTextAttributesKey("GDSCRIPT_METADATA", Default.METADATA)),
-    OPERATION_SIGN(createTextAttributesKey("GDSCRIPT_OPERATION_SIGN", Default.OPERATION_SIGN)),
-    COMMA(createTextAttributesKey("GDSCRIPT_COMMA", Default.COMMA)),
-    DOT(createTextAttributesKey("GDSCRIPT_DOT", Default.DOT)),
-    BRACES(createTextAttributesKey("GDSCRIPT_BRACES", Default.BRACES)),
-    PARENTHESES(createTextAttributesKey("GDSCRIPT_PARENTHESES", Default.PARENTHESES)),
-    BRACKETS(createTextAttributesKey("GDSCRIPT_BRACKETS", Default.BRACKETS)),
-    KEYWORD_FUNCTION_CALL(createTextAttributesKey("GDSCRIPT_KEYWORD_FUNCTION_CALL", Default.KEYWORD)),
-    STATIC_METHOD(createTextAttributesKey("GDSCRIPT_STATIC_METHOD", Default.STATIC_METHOD)),
-    INSTANCE_METHOD(createTextAttributesKey("GDSCRIPT_INSTANCE_METHOD", Default.INSTANCE_METHOD)),
-    CLASS_NAME(createTextAttributesKey("GDSCRIPT_CLASS_NAME", Default.CLASS_NAME)),
-    CONSTANT(createTextAttributesKey("GDSCRIPT_CONSTANT", Default.CONSTANT)),
+    LINE_COMMENT(Default.LINE_COMMENT),
+    STRING(Default.STRING),
+    NUMBER(Default.NUMBER),
+    KEYWORD(Default.KEYWORD),
+    IDENTIFIER(Default.IDENTIFIER),
+    NODE(Default.METADATA),
+    OPERATION_SIGN(Default.OPERATION_SIGN),
+    COMMA(Default.COMMA),
+    DOT(Default.DOT),
+    BRACES(Default.BRACES),
+    PARENTHESES(Default.PARENTHESES),
+    BRACKETS(Default.BRACKETS),
+    KEYWORD_FUNCTION_CALL(Default.KEYWORD),
+    STATIC_METHOD(Default.STATIC_METHOD),
+    INSTANCE_METHOD(Default.INSTANCE_METHOD),
+    CLASS_NAME(Default.CLASS_NAME),
+    CONSTANT(Default.CONSTANT);
+
+    val key: TextAttributesKey =
+        createTextAttributesKey(renameKey(copied.externalName), copied)
+
+    private fun renameKey(copied: String) =
+        copied.replace("DEFAULT", "GDSCRIPT")
 
 }
--- plugin/src/test/kotlin/gdscript/options/ColorTextAttributeKeyTest.kt
+++ plugin/src/test/kotlin/gdscript/options/ColorTextAttributeKeyTest.kt
@@ -1,11 +1,15 @@
 package gdscript.options
 
+import gdscript.options.ColorTextAttributeKey.*
 import junit.framework.TestCase
 
 class ColorTextAttributeKeyTest : TestCase() {
 
     fun `test external name to ensure backwards compatibility`() {
-        assertEquals(ColorTextAttributeKey.LINE_COMMENT.key.externalName, "GDSCRIPT_LINE_COMMENT")
+        assertEquals(LINE_COMMENT.key.externalName, "GDSCRIPT_LINE_COMMENT")
+        assertEquals(IDENTIFIER.key.externalName, "GDSCRIPT_IDENTIFIER")
+        assertEquals(STRING.key.externalName, "GDSCRIPT_STRING")
+        assertEquals(DOT.key.externalName, "GDSCRIPT_DOT")
     }
 
 }
