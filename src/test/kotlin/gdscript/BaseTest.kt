7453e2e9eeb2bd7c6b2b7ece6407c6095a5504b7
Jakub Błach <jakubblach@gmail.com>
2019-10-27 16:56:21 +0100

    Simplify BaseTest implementation

--- src/test/kotlin/gdscript/BaseTest.kt
+++ src/test/kotlin/gdscript/BaseTest.kt
@@ -1,29 +1,28 @@
 package gdscript
 
 import com.intellij.testFramework.UsefulTestCase
-import com.intellij.testFramework.fixtures.*
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.intellij.testFramework.fixtures.IdeaTestExecutionPolicy
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory.getFixtureFactory
+import com.intellij.testFramework.fixtures.TempDirTestFixture
 import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl
 
 abstract class BaseTest : UsefulTestCase() {
 
-    val environment: CodeInsightTestFixture
+    val environment: CodeInsightTestFixture = getFixtureFactory()
+        .createCodeInsightFixture(createFixture(), createTempDir())
 
-    init {
-        val factory = IdeaTestFixtureFactory.getFixtureFactory()
-        val fixtureBuilder = factory.createLightFixtureBuilder(null)
-        val tempDirFixture = createTempDirTestFixture()
-        environment = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(fixtureBuilder.fixture, tempDirFixture)
-    }
-
-    override fun setUp() {
+    override fun setUp() =
         environment.setUp()
-    }
 
-    override fun tearDown() {
+    override fun tearDown() =
         environment.tearDown()
-    }
 
-    private fun createTempDirTestFixture(): TempDirTestFixture {
+    private fun createFixture() = getFixtureFactory()
+        .createLightFixtureBuilder()
+        .fixture
+
+    private fun createTempDir(): TempDirTestFixture {
         val policy = IdeaTestExecutionPolicy.current()
         return policy?.createTempDirTestFixture()
             ?: LightTempDirTestFixtureImpl(true)
