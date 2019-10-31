291527f353f5a0bcb4fcd0adebab0c42c095929b
Jakub Błach <jakubblach@gmail.com>
2019-11-01 00:09:31 +0100

    Add PSI element utility class

--- src/main/kotlin/gdscript/completion/StaticCompletionContributor.kt
+++ src/main/kotlin/gdscript/completion/StaticCompletionContributor.kt
@@ -3,35 +3,29 @@ package gdscript.completion
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
-import com.intellij.psi.PsiComment
-import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.psi.util.PsiTreeUtil
 import gdscript.completion.lookups.ConstantLookups.createConstant
 import gdscript.completion.lookups.InvokeLookups.createStaticMethod
 import gdscript.completion.sources.COMPLETION_DATA
 import gdscript.completion.sources.Class
+import gdscript.completion.utilities.PsiElementUtil.prevLeaf
 
 class StaticCompletionContributor : CompletionContributor() {
 
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
-        val dot = PsiTreeUtil.prevLeaf(parameters.position)
-        if (dot as? LeafPsiElement != null) {
-            if (dot.text == ".") {
-                val id = PsiTreeUtil.prevLeaf(dot)
-                if (id as? LeafPsiElement != null) {
-                    val clazz = findClass(id.text)
-                    if (clazz != null) {
-                        val constants = createConstantLookups(clazz)
-                        result.caseInsensitive().addAllElements(constants)
-                        if (clazz in COMPLETION_DATA.singletons)
-                            result.caseInsensitive().addAllElements(createStaticMethodLookups(clazz))
-                    }
-                }
+        val dot = parameters.position.prevLeaf()
+        if (dot?.text == ".") {
+            val id = dot.prevLeaf()
+            val clazz = findClass(id?.text)
+            if (clazz != null) {
+                val constants = createConstantLookups(clazz)
+                result.caseInsensitive().addAllElements(constants)
+                if (clazz in COMPLETION_DATA.singletons)
+                    result.caseInsensitive().addAllElements(createStaticMethodLookups(clazz))
             }
         }
     }
 
-    private fun findClass(name: String) = (COMPLETION_DATA.classes + COMPLETION_DATA.singletons)
+    private fun findClass(name: String?) = (COMPLETION_DATA.classes + COMPLETION_DATA.singletons)
         .find { it.name == name }
 
     private fun createConstantLookups(clazz: Class) = clazz
