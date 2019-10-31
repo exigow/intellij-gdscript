bc60756561335c5f23ff6ccaaccddbe249f75b06
Jakub Błach <jakubblach@gmail.com>
2019-10-31 21:57:15 +0100

    Change singleton icon for easier distinction / simplify style

--- src/main/kotlin/gdscript/completion/lookups/ConstantLookups.kt
+++ src/main/kotlin/gdscript/completion/lookups/ConstantLookups.kt
@@ -2,22 +2,22 @@ package gdscript.completion.lookups
 
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder.create
-import com.intellij.util.PlatformIcons
+import com.intellij.util.PlatformIcons.*
 
 object ConstantLookups {
 
     fun createConstant(name: String, value: String): LookupElement =
         create(name)
-            .withIcon(PlatformIcons.VARIABLE_ICON)
+            .withIcon(VARIABLE_ICON)
             .withTailText(" = $value")
 
     fun createSingleton(name: String): LookupElement =
         create(name)
-            .withIcon(PlatformIcons.CLASS_ICON)
-            .withItemTextItalic(true)
+            .withIcon(INTERFACE_ICON)
 
     fun createClass(name: String, extends: String): LookupElement =
         create(name)
             .withTypeText(extends)
-            .withIcon(PlatformIcons.CLASS_ICON)
+            .withIcon(CLASS_ICON)
+
 }
