e6add093bee84d8621a9c8143d1923a581298bf6
Jakub Błach <jakubblach@gmail.com>
2019-12-10 23:29:02 +0100

    Re-implement keyword completion contributor (+ fix some issues)

--- src/main/kotlin/gdscript/completion/KeywordCompletionContributor.kt
+++ src/main/kotlin/gdscript/completion/KeywordCompletionContributor.kt
@@ -1,40 +1,44 @@
 package gdscript.completion
 
+import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.completion.AddSpaceInsertHandler.INSTANCE_WITH_AUTO_POPUP
-import com.intellij.codeInsight.completion.CompletionContributor
-import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.AutoCompletionPolicy
 import com.intellij.codeInsight.lookup.LookupElementBuilder.create
-import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.util.ProcessingContext
 import gdscript.completion.sources.CompletionUtils
 
 
 class KeywordCompletionContributor : CompletionContributor() {
 
-    override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
-        val prevLeaf = PsiTreeUtil.prevLeaf(parameters.position)
-        when (prevLeaf?.text) {
-            "\n" -> result.addAllElements(STATEMENT_LOOKUPS)
-            "export" -> result.addElement(VAR_LOOKUP)
-            "static" -> result.addElement(FUNC_LOOKUP)
-        }
+    init {
+        extend(CompletionType.BASIC, psiElement().afterLeaf("\n"), KeywordCompletionProvider(CompletionUtils.keywordsStatements()))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("export"), KeywordCompletionProvider(listOf("var")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("onready"), KeywordCompletionProvider(listOf("var")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("master"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("master"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("mastersync"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("puppet"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("puppetsync"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("remote"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("remotesync"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("sync"), KeywordCompletionProvider(listOf("var", "func")))
+        extend(CompletionType.BASIC, psiElement().afterLeaf("static"), KeywordCompletionProvider(listOf("func")))
     }
 
-    private companion object {
+    class KeywordCompletionProvider(private val texts: List<String>) : CompletionProvider<CompletionParameters>() {
 
-        private val VAR_LOOKUP = createKeywordSpaced("var")
+        override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+            val lookups = createLookups()
+            result.addAllElements(lookups)
+        }
 
-        private val FUNC_LOOKUP = createKeywordSpaced("func")
-
-        private val STATEMENT_LOOKUPS =
-            CompletionUtils.keywordsStatements()
-                .map { createKeywordSpaced(it) }
-
-        private fun createKeywordSpaced(name: String) =
-            create(name)
+        private fun createLookups() = texts.map {
+            create(it)
                 .withInsertHandler(INSTANCE_WITH_AUTO_POPUP)
                 .bold()
-
+                .withAutoCompletionPolicy(AutoCompletionPolicy.ALWAYS_AUTOCOMPLETE)
+        }
     }
 
 }
--- src/test/kotlin/gdscript/completion/KeywordCompletionContributorTest.kt
+++ src/test/kotlin/gdscript/completion/KeywordCompletionContributorTest.kt
@@ -6,14 +6,24 @@ import uitlities.openCode
 
 class KeywordCompletionContributorTest : BaseTest() {
 
-    fun `test complete "var" keyword after new line"`() {
+    fun `test newline var`() {
         environment.openCode("\nva<caret>")
         assertTrue("var" in environment.lookupTexts())
     }
 
-    fun `test complete "static" keyword after new line`() {
+    fun `test newline const`() {
+        environment.openCode("\ncon<caret>")
+        assertTrue("const" in environment.lookupTexts())
+    }
+
+    fun `test newline static`() {
         environment.openCode("\nsta<caret>")
         assertTrue("static" in environment.lookupTexts())
+    }
+
+    fun `test newline class_name`() {
+        environment.openCode("\nclas<caret>")
+        assertTrue("class_name" in environment.lookupTexts())
     }
 
 }
