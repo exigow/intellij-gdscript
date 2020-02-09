0f57b894e178889049947e32e747de0a79a356c7
Jakub Błach <jakubblach@gmail.com>
2020-02-09 17:52:07 +0100

    Simplify script quote handler

--- src/main/kotlin/gdscript/ScriptQuoteHandler.kt
+++ src/main/kotlin/gdscript/ScriptQuoteHandler.kt
@@ -2,27 +2,29 @@ package gdscript
 
 
 import com.intellij.codeInsight.editorActions.MultiCharQuoteHandler
-import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
+import com.intellij.codeInsight.editorActions.QuoteHandler
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.highlighter.HighlighterIterator
-import gdscript.ScriptLexer.STRING_APHOSTROPHE
-import gdscript.ScriptLexer.STRING_DOUBLE_QUOTE
-import org.antlr.intellij.adaptor.lexer.TokenIElementType
+import com.intellij.openapi.util.TextRange
 
+class ScriptQuoteHandler : QuoteHandler, MultiCharQuoteHandler {
 
-class ScriptQuoteHandler : SimpleTokenSetQuoteHandler(ScriptTokenSet.STRINGS), MultiCharQuoteHandler {
-
-    override fun getClosingQuote(iterator: HighlighterIterator, offset: Int): CharSequence? {
-        val token = recognizeTokenOnPosition(iterator)
-        return matchClosingCharacter(token)
+    override fun isClosingQuote(iterator: HighlighterIterator?, offset: Int): Boolean {
+        val prev = iterator?.document?.text?.get(offset)
+            ?: return false
+        return prev != '\'' && prev != '\"'
     }
 
-    private fun matchClosingCharacter(token: Int?) = when (token) {
-        STRING_DOUBLE_QUOTE -> "\""
-        STRING_APHOSTROPHE -> "\'"
-        else -> null
-    }
+    override fun getClosingQuote(iterator: HighlighterIterator, offset: Int): CharSequence? =
+        iterator.document.getText(TextRange(offset - 1, offset))
 
-    private fun recognizeTokenOnPosition(iterator: HighlighterIterator) =
-        (iterator.tokenType as? TokenIElementType)?.antlrTokenType
+    override fun isOpeningQuote(iterator: HighlighterIterator?, offset: Int) =
+        true
+
+    override fun hasNonClosedLiteral(editor: Editor?, iterator: HighlighterIterator?, offset: Int) =
+        true
+
+    override fun isInsideLiteral(iterator: HighlighterIterator?) =
+        true
 
 }
