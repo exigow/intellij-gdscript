dc47c0bd66d652a183d2e1250c86f45ee1784f5f
Jakub Błach <jakubblach@gmail.com>
2020-05-14 17:47:16 +0200

    Simplify function annotation

--- src/main/kotlin/gdscript/annotation/MethodAnnotator.kt
+++ src/main/kotlin/gdscript/annotation/MethodAnnotator.kt
@@ -3,18 +3,19 @@ package gdscript.annotation
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import common.Colors.INSTANCE_METHOD
-import common.PsiElementUtils.hasAncestors
 import common.PsiElementUtils.isToken
 import gdscript.ScriptLexer.IDENTIFIER
-import gdscript.ScriptParser.RULE_funcLine
-import gdscript.ScriptParser.RULE_id
 
 class MethodAnnotator : Annotator {
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element.isToken(IDENTIFIER) && element.hasAncestors(RULE_id, RULE_funcLine))
+        if (element.isToken(IDENTIFIER) && isAfterFuncKeyword(element))
             holder.colorize(element, INSTANCE_METHOD)
     }
+
+    private fun isAfterFuncKeyword(element: PsiElement) =
+        PsiTreeUtil.prevVisibleLeaf(element)?.text == "func"
 
 }
