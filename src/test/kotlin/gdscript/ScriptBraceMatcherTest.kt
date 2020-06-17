eab5f46c2fc3421bd408f71d022e1943b695795b
Jakub Błach <jakubblach@gmail.com>
2020-06-17 17:52:39 +0200

    Add more brace matcher tests

--- src/test/kotlin/gdscript/ScriptBraceMatcherTest.kt
+++ src/test/kotlin/gdscript/ScriptBraceMatcherTest.kt
@@ -5,22 +5,40 @@ import utils.openScript
 
 class ScriptBraceMatcherTest : BaseTest() {
 
-    fun `test curly braces`() {
+    fun `test add closing curly brace`() {
         environment.openScript("x = <caret>")
         environment.type('{')
         environment.checkResult("x = {<caret>}")
     }
 
-    fun `test square braces`() {
+    fun `test remove both curly braces`() {
+        environment.openScript("x = {<caret>}")
+        environment.type('\b')
+        environment.checkResult("x = <caret>")
+    }
+
+    fun `test add closing square brace`() {
         environment.openScript("x = <caret>")
         environment.type('[')
         environment.checkResult("x = [<caret>]")
     }
 
-    fun `test round braces`() {
+    fun `test remove both square braces`() {
+        environment.openScript("x = [<caret>]")
+        environment.type('\b')
+        environment.checkResult("x = <caret>")
+    }
+
+    fun `test add closing round brace`() {
         environment.openScript("x = <caret>")
         environment.type('(')
         environment.checkResult("x = (<caret>)")
+    }
+
+    fun `test remove both round braces`() {
+        environment.openScript("x = (<caret>)")
+        environment.type('\b')
+        environment.checkResult("x = <caret>")
     }
 
 }
