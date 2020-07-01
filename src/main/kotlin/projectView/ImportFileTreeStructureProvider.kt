fc92ee5e64c6ed3680f84078939a15ff84168f21
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2020-07-01 18:53:31 +0300

    Replace implementation with TreeStructureProvider for dynamic nesting
    It appears that the NestingRulesProvider really only works nicely when
    there are a static set of rules to enforce. Since we want to nest any
    file which has a corresponding .import file as a sibling it seems easier
    to handle this in a more manual fashion.

---
+++ src/main/kotlin/projectView/ImportFileTreeStructureProvider.kt
@@ -0,0 +1,57 @@
+package projectView
+
+import com.intellij.ide.projectView.ProjectViewSettings
+import com.intellij.ide.projectView.TreeStructureProvider
+import com.intellij.ide.projectView.ViewSettings
+import com.intellij.ide.projectView.impl.nodes.NestingTreeNode
+import com.intellij.ide.projectView.impl.nodes.PsiFileNode
+import com.intellij.ide.util.treeView.AbstractTreeNode
+
+class ImportFileTreeStructureProvider : TreeStructureProvider {
+
+    override fun modify(
+        parent: AbstractTreeNode<*>,
+        children: MutableCollection<AbstractTreeNode<Any>>,
+        settings: ViewSettings?
+    ): MutableCollection<AbstractTreeNode<Any>> {
+        if (settings is ProjectViewSettings && !settings.isUseFileNestingRules) {
+            return children
+        }
+
+        val newChildren = mutableListOf<AbstractTreeNode<Any>>()
+        val nodesByFilename = children
+            .mapNotNull { child -> child as? PsiFileNode }
+            .associateBy { child -> child.virtualFile?.name }
+
+        for (child in children) {
+            if (child !is PsiFileNode) {
+                newChildren.add(child)
+                continue
+            }
+
+            val filename = child.virtualFile?.name
+
+            if (filename == null) {
+                newChildren.add(child)
+                continue
+            }
+
+            // Skip "x.png.import" when "x.png" is a sibling
+            if (filename.endsWith(".import") && filename.removeSuffix(".import") in nodesByFilename) {
+                continue
+            }
+
+            val siblingToNest = nodesByFilename["$filename.import"]
+
+            // Replace "x.png" with a nested node containing "x.png.import"
+            if (siblingToNest != null) {
+                val nested = NestingTreeNode(child, arrayListOf(siblingToNest))
+                newChildren.add(nested as AbstractTreeNode<Any>)
+            } else {
+                newChildren.add(child)
+            }
+        }
+
+        return newChildren
+    }
+}
