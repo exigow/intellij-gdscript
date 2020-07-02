bbfc0f2003d8c42dfc8cea0b50b5f7651ac9040c
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2020-07-02 17:03:26 +0300

    Add test to assert .import file nesting obeys configuration variable

---
+++ src/test/kotlin/projectView/ImportFileTreeStructureProviderTest.kt
@@ -0,0 +1,73 @@
+package projectView
+
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
+import com.intellij.projectView.TestProjectTreeStructure
+import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import utils.addFile
+
+class ImportFileTreeStructureProviderTest : BasePlatformTestCase() {
+
+    fun `test import files are nested by default`() {
+        val projectView = ProjectView.getInstance(project)
+        val structure = CustomProjectTreeStructure(
+            project,
+            testRootDisposable,
+            useFileNestingRules = true
+        )
+        val pane = structure.createPane()
+        projectView.addProjectPane(pane)
+
+        myFixture.addFile("my_texture.png")
+        myFixture.addFile("my_texture.png.import")
+
+        PlatformTestUtil.expandAll(pane.tree)
+
+        val expected = """
+            -Project
+             -PsiDirectory: src
+              -my_texture.png
+               my_texture.png.import
+             External Libraries
+        """.trimIndent()
+        PlatformTestUtil.assertTreeEqual(pane.tree, expected)
+    }
+
+    fun `test import files are not nested if configured`() {
+        val projectView = ProjectView.getInstance(project)
+        val structure = CustomProjectTreeStructure(
+            project,
+            testRootDisposable,
+            useFileNestingRules = false
+        )
+        val pane = structure.createPane()
+        projectView.addProjectPane(pane)
+
+        myFixture.addFile("my_texture.png")
+        myFixture.addFile("my_texture.png.import")
+
+        PlatformTestUtil.expandAll(pane.tree)
+
+        val expected = """
+            -Project
+             -PsiDirectory: src
+              my_texture.png
+              my_texture.png.import
+             External Libraries
+        """.trimIndent()
+        PlatformTestUtil.assertTreeEqual(pane.tree, expected)
+    }
+
+    private class CustomProjectTreeStructure(
+        project: Project,
+        parentDispoable: Disposable,
+        val useFileNestingRules: Boolean
+    ) : TestProjectTreeStructure(project, parentDispoable) {
+
+        override fun isUseFileNestingRules(): Boolean {
+            return useFileNestingRules
+        }
+    }
+}
