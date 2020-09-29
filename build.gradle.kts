65b46c2edf655abd5b81b025d0653a316e2d9f3b
Jakub Błach <jakub.blach@kontomatik.com>
2020-09-29 22:19:38 +0200

    Fix compilation warnings related to kotlin-reflect version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,26 @@
 plugins {
     java
-    id("org.jetbrains.kotlin.jvm").version("1.4.10")
-    id("org.jetbrains.intellij").version("0.4.21")
+    id("org.jetbrains.kotlin.jvm").version("1.3.70")
+    id("org.jetbrains.intellij").version("0.4.26")
 }
 
 dependencies {
     implementation("org.jsoup:jsoup:1.13.1")
     implementation("net.pearx.kasechange:kasechange-jvm:1.3.0")
     implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.9.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.70")
     testImplementation("io.mockk:mockk:1.10.2")
 }
 
 repositories {
     jcenter()
     mavenCentral()
+}
+
+buildscript {
+    repositories {
+        maven("https://jetbrains.bintray.com/intellij-plugin-service")
+    }
 }
 
 intellij {
@@ -26,10 +32,6 @@ tasks {
     publishPlugin {
         token(project.property("publishPluginToken"))
         channels("stable")
-    }
-    compileJava {
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
     }
     compileKotlin {
         kotlinOptions {
