addb7a646dd6995f2505aee8bf9a8a9d5d0d7645
Jakub Błach <jakub.blach@kontomatik.com>
2020-09-29 21:23:55 +0200

    Upgrade Kotlin, Gradle and clean dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,15 @@
 plugins {
     java
-    id("org.jetbrains.kotlin.jvm").version("1.3.72")
+    id("org.jetbrains.kotlin.jvm").version("1.4.10")
     id("org.jetbrains.intellij").version("0.4.21")
 }
 
 dependencies {
-    implementation("org.jsoup:jsoup:1.7.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.9.9")
-    implementation("net.pearx.kasechange:kasechange-jvm:1.1.0")
+    implementation("org.jsoup:jsoup:1.13.1")
+    implementation("net.pearx.kasechange:kasechange-jvm:1.3.0")
     implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.9.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.72")
-    testImplementation("io.mockk:mockk:1.9.3")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
+    testImplementation("io.mockk:mockk:1.10.2")
 }
 
 repositories {
