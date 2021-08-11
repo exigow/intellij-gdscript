241e9fa165554bfc364671c641f446bc96cbf54e
Jakub Blach <jakubblach@gmail.com>
2021-08-11 22:03:10 +0200

    Simplify TscnReferenceContributor.kt

--- src/main/kotlin/tscn/TscnReferenceContributor.kt
+++ src/main/kotlin/tscn/TscnReferenceContributor.kt
@@ -1,9 +1,11 @@
 package tscn
 
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.psi.*
 import com.intellij.util.ProcessingContext
-import tscn.psi.*
+import tscn.psi.TscnExtExpression
+import tscn.psi.TscnResourceString
+import tscn.psi.TscnSubExpression
 import tscn.psi.reference.TscnResourceExtReference
 import tscn.psi.reference.TscnResourceStringReference
 import tscn.psi.reference.TscnResourceSubReference
@@ -11,31 +13,22 @@ import tscn.psi.reference.TscnResourceSubReference
 class TscnReferenceContributor : PsiReferenceContributor() {
 
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
-        registrar.registerReferenceProvider(PlatformPatterns.psiElement(TscnResourceString::class.java), object : PsiReferenceProvider() {
+        registrar.registerReferenceProvider(psiElement(TscnResourceString::class.java), object : PsiReferenceProvider() {
 
-            override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-                if (element is TscnResourceString)
-                    return arrayOf(TscnResourceStringReference(element))
-                return emptyArray()
-            }
+            override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> =
+                arrayOf(TscnResourceStringReference(element as TscnResourceString))
 
         })
-        registrar.registerReferenceProvider(PlatformPatterns.psiElement(TscnExtExpression::class.java), object : PsiReferenceProvider() {
+        registrar.registerReferenceProvider(psiElement(TscnExtExpression::class.java), object : PsiReferenceProvider() {
 
-            override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-                if (element is TscnExtExpression)
-                    return arrayOf(TscnResourceExtReference(element))
-                return emptyArray()
-            }
+            override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> =
+                arrayOf(TscnResourceExtReference(element as TscnExtExpression))
 
         })
-        registrar.registerReferenceProvider(PlatformPatterns.psiElement(TscnSubExpression::class.java), object : PsiReferenceProvider() {
+        registrar.registerReferenceProvider(psiElement(TscnSubExpression::class.java), object : PsiReferenceProvider() {
 
-            override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-                if (element is TscnSubExpression)
-                    return arrayOf(TscnResourceSubReference(element))
-                return emptyArray()
-            }
+            override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> =
+                arrayOf(TscnResourceSubReference(element as TscnSubExpression))
 
         })
     }
